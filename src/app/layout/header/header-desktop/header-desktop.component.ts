a9a5e5079981791b757a53ce49286a74bbb670be
AnnieAlig <coolahhy@gmail.com>
2019-02-21 11:24:59 +0200

    homepage edits

--- src/app/layout/header/header-desktop/header-desktop.component.ts
+++ src/app/layout/header/header-desktop/header-desktop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef, ViewChild } from '@angular/core';
 import {DialogService} from 'ng2-bootstrap-modal';
 import { SearchModalComponent } from '../../../shared/modals/search-modal/search-modal.component';
 
@@ -9,7 +9,19 @@ import { SearchModalComponent } from '../../../shared/modals/search-modal/search
               '../header.component.scss']
 })
 export class HeaderDesktopComponent implements OnInit {
+  @ViewChild('dropdown_menu') private dropdown: ElementRef;
+  @ViewChild('dropdown_link') private dropdown_link: ElementRef;
+  private collapsed;
 
+  @HostListener('document:click', ['$event'])
+  clickout(event) {
+    if (this.collapsed) {
+      if (this.dropdown && !this.dropdown.nativeElement.contains(event.target)
+        && !this.dropdown_link.nativeElement.contains(event.target)) {
+        this.collapsed = !this.collapsed;
+      }
+    }
+  }
   constructor(
     private dialogService: DialogService,
   ) { }
